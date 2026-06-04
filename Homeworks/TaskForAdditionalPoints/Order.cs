104c99cf1b00ebce853d95fca4d3a2641a9f287e
SivoplyasMaksim <maksimsivoplyas220906@gmail.com>
2026-06-05 00:06:19 +0300

    add task

---
+++ Homeworks/TaskForAdditionalPoints/Order.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskForAdditionalPoints
+{
+    public class Order
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString()[..8];
+
+        [Required]
+        public string UserEmail { get; set; }
+
+        [Required]
+        public decimal TotalAmount { get; set; }
+
+        public string Status { get; set; } 
+        public decimal FinalAmount { get; set; }
+
+        public Order(string email, decimal amount, string status)
+        {
+            UserEmail = email;
+            TotalAmount = amount;
+            Status = status;
+            FinalAmount = amount;
+        }
+    }
+}
---
+++ Homeworks/TaskForAdditionalPoints/StudentDiscount.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskForAdditionalPoints
+{
+    public class StudentDiscount : IDiscountRule
+    {
+        public void ApplyDiscount(Order order)
+        {
+            if (order.UserEmail.EndsWith("kpfu.ru"))
+            {
+                order.FinalAmount *= 0.95m; 
+                Console.WriteLine($"Применена скидка студента КФУ для заказа {order.Id}");
+            }
+        }
+    }
+}
