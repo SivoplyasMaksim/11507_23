f2171e3ef34a58fe699f34e6f037a375d4b6190a
SivoplyasMaksim <maksimsivoplyas220906@gmail.com>
2026-06-04 23:57:31 +0300

    1

--- Homeworks/TaskForAdditionalPoints/Order.cs
+++ Homeworks/TaskForAdditionalPoints/Order.cs
@@ -2,9 +2,7 @@
 
 namespace TaskForAdditionalPoints
 {
-    // ==========================================
-    // МОДЕЛИ ДАННЫХ
-    // ==========================================
+
     public class Order
     {
         public string Id { get; set; } = Guid.NewGuid().ToString()[..8];
